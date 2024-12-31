bf9a873126e2280964df55fd9b971735bb0e46f0
EslamKamel89 <elsamkamelforex@gmail.com>
2024-12-31 09:19:55 +0200

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default {
+  setup() {
+    const message = 'Hello world'
+    return {
+      message,
+    }
+  },
+}
+</script>
+
+<template>
+  <h2>{{ message }}</h2>
+</template>
+
+<style></style>
